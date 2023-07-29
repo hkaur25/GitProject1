e351cbc7a277f32b7a15cfef53a14609224f290a
harka <harka@Soni.mynetworksettings.com>
2023-07-29 10:32:02 -0400

    Implemented add and subtract method

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -6,5 +6,15 @@ public class DemoDay2 {
 		
 		System.out.println("Test");
 	}
+	
+	//ADD METHOD
+	public static void add (int a, int b) {
+		System.out.println(a+ b);
+	}
+	
+	//subtract method
+	public static void subtact(int a, int b) {
+		System.out.println(a-b);
+	}
 
 }
