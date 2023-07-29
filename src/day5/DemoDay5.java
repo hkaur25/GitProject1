81eebf0393dffb95ba30174df032e237c9c511d6
harka <harka@Soni.mynetworksettings.com>
2023-07-29 13:27:45 -0400

    added getData()

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -4,6 +4,10 @@ public class DemoDay5 {
 	public static void main(String[] args) {
 		System.out.println("It's mostly Sunny with 89F");
 	}
+	
+	public void getInfo() {
+		System.out.println("Weather");
+	}
 
 	public void getData(){
 		System.out.println("the temperature");
